6211d29f7c0eb76dc56b35c8459bffe36eee3e00
Remyasree-P <remyasreep001@gmail.com>
2024-08-13 17:04:49 +0530

    files updated

--- script.js
+++ script.js
@@ -36,3 +36,44 @@ radios.forEach((radio, i) => {
 });
 
 showItem(currentIndex);
+
+let cityName = document.getElementById('cityName');
+let cityDetails = document.getElementById('cityDetails');
+let temperature = document.getElementById('temperature');
+let date = document.getElementById('date');
+
+// Function to fetch weather details
+let fetchDetails = async (city = cityName.value) => {
+    if (city.trim().length == 0) {
+        alert("Please enter a city name before searching");
+    } else {
+        try {
+            let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=1cb6532aea3c298a830a71380eace21e`;
+            let response = await fetch(url);
+            if (!response.ok) {
+                throw new Error("City not found");
+            }
+            let data = await response.json();
+            cityDetails.innerText = `${data.name}, ${data.sys.country}`;
+            
+            let x = new Date();
+            date.innerText = `📅 ${x.toDateString()}`;
+            
+            let temp = data.main.temp;
+            if (temp > 30) {
+                temperature.innerHTML = `${temp}<sup>o</sup>C 🔥`;
+            } else if (temp >= 20) {
+                temperature.innerHTML = `${temp}<sup>o</sup>C 🌞`;
+            } else if (temp > 0) {
+                temperature.innerHTML = `${temp}<sup>o</sup>C ❄️`;
+            } else {
+                temperature.innerHTML = `${temp}<sup>o</sup>C ⛄`;
+            }
+        } catch (error) {
+            alert(error.message);
+        }
+    }
+}
+window.onload = () => {
+    fetchDetails("Bangalore");
+};
